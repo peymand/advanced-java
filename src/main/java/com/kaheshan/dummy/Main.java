1c9e056fb8e412462e8f77111390ab6e14f3bcf8
peyma <peymandodangeh@gmail.com>
2020-09-03 11:45:05 +0430

    افزودن پرینت

--- src/main/java/com/kaheshan/dummy/Main.java
+++ src/main/java/com/kaheshan/dummy/Main.java
@@ -3,5 +3,6 @@ package com.kaheshan.dummy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Spring");
+        System.out.println("hello");
     }
 }

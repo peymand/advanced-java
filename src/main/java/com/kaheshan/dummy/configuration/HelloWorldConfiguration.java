2c35ba5642ae405f839396e3160b40cf3a368f90
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-09-10 07:13:37 +0430

    spring mvc init step

--- src/main/java/com/kaheshan/dummy/configuration/HelloWorldConfiguration.java
+++ src/main/java/com/kaheshan/dummy/configuration/HelloWorldConfiguration.java
@@ -5,15 +5,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.kaheshan.dummy")
-public class HelloWorldConfiguration {
-	
-	@Bean(name="HelloWorld")
+public class HelloWorldConfiguration implements WebMvcConfigurer{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+	}
+
+
+	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);

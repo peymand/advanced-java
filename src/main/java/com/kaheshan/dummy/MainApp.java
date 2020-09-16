080c14998f6e5e1061f1402d9af68c2d868ade18
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-09-16 22:00:50 +0430

    hibernate spring core

---
+++ src/main/java/com/kaheshan/dummy/MainApp.java
@@ -0,0 +1,41 @@
+package com.kaheshan.dummy;
+
+
+import com.kaheshan.dummy.config.HibernateConfig;
+import com.kaheshan.dummy.model.Person;
+import com.kaheshan.dummy.service.PersonService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author imssbora
+ *
+ */
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(HibernateConfig.class);
+
+      PersonService personService = context.getBean(PersonService.class);
+
+      // Add Persons
+      personService.add(new Person("Sunil", "Bora", "suni.bora@example.com"));
+      personService.add(new Person("David", "Miller", "david.miller@example.com"));
+      personService.add(new Person("Sameer", "Singh", "sameer.singh@example.com"));
+      personService.add(new Person("Paul", "Smith", "paul.smith@example.com"));
+
+      // Get Persons
+      List<Person> persons = personService.listPersons();
+      for (Person person : persons) {
+         System.out.println("Id = "+person.getId());
+         System.out.println("First Name = "+person.getFirstName());
+         System.out.println("Last Name = "+person.getLastName());
+         System.out.println("Email = "+person.getEmail());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/com/kaheshan/dummy/config/HibernateConfig.java
@@ -0,0 +1,80 @@
+package com.kaheshan.dummy.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:database.properties")
+public class HibernateConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[]{"com.kaheshan.dummy"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+
+        HikariConfig config = new HikariConfig();
+        HikariDataSource ds;
+
+        config.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
+        config.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        config.setUsername(environment.getRequiredProperty("jdbc.username"));
+        config.setPassword(environment.getRequiredProperty("jdbc.password"));
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.addDataSourceProperty("maximumPoolSize", "20");
+        ds = new HikariDataSource(config);
+
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return properties;
+    }
+}
+

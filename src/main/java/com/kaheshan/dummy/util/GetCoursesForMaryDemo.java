748fb59f985f9e7c5c28bc5a45ac896b584021c5
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-09-16 23:05:56 +0430

    hibernate spring core

---
+++ src/main/java/com/kaheshan/dummy/util/GetCoursesForMaryDemo.java
@@ -0,0 +1,56 @@
+package com.kaheshan.dummy.util;
+
+import com.kaheshan.dummy.model.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCoursesForMaryDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+				
+			// get the student from database
+			int studentId = 1;
+			Student tempStudent = session.get(Student.class, studentId);
+			
+			System.out.println("\nLoaded student: " + tempStudent);
+			System.out.println("Courses: " + tempStudent.getCourses());		
+						
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+

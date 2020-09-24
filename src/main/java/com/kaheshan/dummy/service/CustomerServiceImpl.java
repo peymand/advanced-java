4d20e46a5f1bfd350cc957ce28a9a7c2acf5e0b8
peyma <peymandodangeh@gmail.com>
2020-09-24 13:14:09 +0330

    transactional annotation added

--- src/main/java/com/kaheshan/dummy/service/CustomerServiceImpl.java
+++ src/main/java/com/kaheshan/dummy/service/CustomerServiceImpl.java
@@ -8,10 +8,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
 	// need to inject the session factory

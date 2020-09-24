80ad403b57370bcbd789cd1ab5eba4e57ec54e9f
peyma <peymandodangeh@gmail.com>
2020-09-24 13:02:56 +0330

    adding model attribute method

--- src/main/java/com/kaheshan/dummy/controller/StudentController.java
+++ src/main/java/com/kaheshan/dummy/controller/StudentController.java
@@ -1,21 +1,24 @@
 package com.kaheshan.dummy.controller;
 
 import com.kaheshan.dummy.model.Student;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
 	@RequestMapping("/showForm")
-	public String showForm(Model theModel) {
+	public String showForm(Model theModel, Student theStudent) {
 		
 		// create a student object
-		Student theStudent = new Student();
-		
+//		Student theStudent = new Student();
 		// add student object to the model
 		theModel.addAttribute("student", theStudent);
 		
@@ -30,6 +33,11 @@ public class StudentController {
 							+ " " + theStudent.getLastName());
 		
 		return "student-confirmation";
+	}
+
+	@ModelAttribute
+	public void addStudentToModel(Model model){
+		model.addAttribute("student",new Student());
 	}
 	
 }

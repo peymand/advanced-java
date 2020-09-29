0bf4e310c5635b81d6ae9b1eacc575317436a46e
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-09-29 21:57:27 +0330

    new branch for rest course

--- src/main/java/com/kaheshan/dummy/controller/HomeController.java
+++ src/main/java/com/kaheshan/dummy/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.kaheshan.dummy.controller;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +13,9 @@ public class HomeController {
 	private SessionFactory sessionFactory;
 
 	@RequestMapping("/")
-	public String showPage() {
+	public String showPage(@RequestBody(required = false) String s)
+	{
+		System.out.println(s);
 		return "main-menu";
 	}
 }

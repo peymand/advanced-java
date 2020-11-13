be452e15b3373a933993b76bf36b32377ed3e562
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-11-13 18:01:44 +0330

    init project through spring initializer

--- README.md
+++
@@ -1,1 +0,0 @@
-this repository is created to store all codes and samples in java advenced course in kahkeshan nour by me.
--- src/main/java/com/kaheshan/dummy/configuration/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.kaheshan.dummy.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}

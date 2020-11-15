1d78be05c5d8d6c100435fbb7a4778c93c40b8f9
peyma <peymandodangeh@gmail.com>
2020-11-15 18:55:57 +0330

    some change in security config

--- src/main/java/com/peyman/springboot/rest/FunRestController.java
+++ src/main/java/com/peyman/springboot/rest/FunRestController.java
@@ -4,15 +4,18 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest")
 public class FunRestController {
 		
 	// expose "/" that return "Hello World"
 	
 	@GetMapping("/")
 	public String sayHello() {
+
 		return "Hello World! Time on server is " + LocalDateTime.now();
 	}
 	

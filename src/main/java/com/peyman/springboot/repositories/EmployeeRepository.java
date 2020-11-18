529823d915b3d9f1f6475da43288b9eadc5a3cb3
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-11-18 20:19:21 +0330

    adding spring data rest  dao to project

--- src/main/java/com/peyman/springboot/repositories/EmployeeRepository.java
+++ src/main/java/com/peyman/springboot/repositories/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.peyman.springboot.repositories;
 
 import com.peyman.springboot.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!

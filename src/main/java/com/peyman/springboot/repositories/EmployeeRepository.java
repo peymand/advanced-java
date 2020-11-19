0c235beb8175b7e768e816ec750d6602cde1a715
peyma <peymandodangeh@gmail.com>
2020-11-19 13:31:00 +0330

    some code added in class

--- src/main/java/com/peyman/springboot/repositories/EmployeeRepository.java
+++ src/main/java/com/peyman/springboot/repositories/EmployeeRepository.java
@@ -4,8 +4,10 @@ package com.peyman.springboot.repositories;
 import com.peyman.springboot.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path = "members")
+//@RepositoryRestResource(path = "members")
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!

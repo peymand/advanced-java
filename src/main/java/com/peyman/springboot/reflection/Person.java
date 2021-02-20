781427f46e0550f93f2c741fe7c9e00a4f967898
peyma <peymandodangeh@gmail.com>
2021-02-20 18:20:23 +0330

    adding some code about annotations

--- src/main/java/com/peyman/springboot/reflection/Person.java
+++ src/main/java/com/peyman/springboot/reflection/Person.java
@@ -3,4 +3,10 @@ package com.peyman.springboot.reflection;
 public class Person {
     private String name;
     private int age;
+
+//    public int sum(int a, int b){
+//
+//        if(a!=b){}
+//        return a+b-1;
+//    }
 }
---
+++ src/test/java/com/peyman/springboot/reflection/PersonTest.java
@@ -0,0 +1,16 @@
+package com.peyman.springboot.reflection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void sum_should_return_sum_of_twoIntegers() {
+        Person p = new Person();
+//        int result = p.sum(2,3);
+//        assertEquals(5,result);
+
+    }
+}

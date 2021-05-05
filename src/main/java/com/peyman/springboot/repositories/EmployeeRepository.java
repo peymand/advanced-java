56d7a750e5c68fa85a74b0547ae4890e39ffb1fd
peyman <peymandodangeh@gmail.com>
2021-05-05 15:37:57 +0430

    adding some code about annotations

--- src/main/java/com/peyman/springboot/repositories/EmployeeRepository.java
+++ src/main/java/com/peyman/springboot/repositories/EmployeeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-//@RepositoryRestResource(path = "members")
+@RepositoryRestResource(path = "members")
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 

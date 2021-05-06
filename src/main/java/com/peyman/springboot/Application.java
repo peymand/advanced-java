fd4d8df55f9c4cabb14cb0e12af3caaf66641d65
peyman <peymandodangeh@gmail.com>
2021-05-06 11:15:55 +0430

    adding some code about annotations

--- src/main/java/com/peyman/springboot/Application.java
+++ src/main/java/com/peyman/springboot/Application.java
@@ -2,6 +2,7 @@ package com.peyman.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -9,6 +10,8 @@ public class Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
+
+
 	}
 
 }
